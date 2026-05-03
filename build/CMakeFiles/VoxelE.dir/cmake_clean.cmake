dcaa57bd595d97b115d9fe48dba7fa390b1fa485
Leonuraht <blinderoggy@google.com>
2026-05-03 22:02:34 +0530

    ...

---
+++ build/CMakeFiles/VoxelE.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VoxelE.dir/link.d"
+  "CMakeFiles/VoxelE.dir/src/main.cpp.o"
+  "CMakeFiles/VoxelE.dir/src/main.cpp.o.d"
+  "VoxelE"
+  "VoxelE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VoxelE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
